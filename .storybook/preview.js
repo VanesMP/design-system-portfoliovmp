93900786db1e8271219044b209395e3c01bc9e91
Vanessa Macedo <vanessa@MacBook-Pro-de-Vanessa.local>
2026-04-20 21:00:23 +0200

    add color palette docs

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,6 +3,7 @@ import "../dist/css/dev-light.css";
 import "../dist/css/dev-dark.css";
 import "../dist/css/art-light.css";
 import "../dist/css/art-dark.css";
+import "../docs.scss";
 
 const preview = {
   parameters: {
